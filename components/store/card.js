545d12ce55991b3da757262a6543e97d6f195d7b
Blake Jon Mycal Smith <B.jonmycal@gmail.com>
2024-07-02 12:19:19 -0400

    all stores view is working

--- components/store/card.js
+++ components/store/card.js
@@ -11,7 +11,7 @@ export function StoreCard({ store, width= "is-half" }) {
         </header>
         <div className="card-content">
           <p className="content">
-            Owner: {store.seller.first_name} {store.seller.last_name}
+            Owner: {store.customer.user.first_name} {store.customer.user.last_name}
           </p>
           <div className="content">
             {store.description}
