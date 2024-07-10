ec10dbc39e6066c0c15c23dcaf4af440a6fa6c40
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-07-10 15:43:38 -0500

    Add rating/review functionality

--- components/rating/container.js
+++ components/rating/container.js
@@ -1,10 +1,10 @@
 import { RatingCard } from './card'
 import RatingForm from './form'
 
-export function RatingsContainer({ ratings, saveRating }) {
+export function RatingsContainer({ ratings, saveRating, productId }) {
   return (
     <div className="tile is-parent is-12 is-vertical container">
-      <RatingForm saveRating={saveRating} />
+      <RatingForm saveRating={saveRating} productId={productId}/>
       {
         ratings?.map((rating) => <RatingCard key={rating.id} rating={rating} />)
       }
